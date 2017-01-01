8c05e3b0120c6e760550416d674097e836a0bd7d
pat <pp.psuwit@gmail.com>
2017-01-01 02:40:27 -0700

    Working version of map displaying from google sheet!

--- js/earth_initialize.js
+++ js/earth_initialize.js
@@ -1,28 +1,33 @@
-      function initialize() {
-        var earth = new WE.map('earth_div');
-        WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(earth);
-
-        var marker = WE.marker([51.5, -0.09]).addTo(earth);
-        marker.bindPopup("<b>Hello world!</b><br>I am a popup.<br /><span style='font-size:10px;color:#999'>Tip: Another popup is hidden in Cairo..</span>", {maxWidth: 150, closeButton: true}).openPopup();
-
-        var marker2 = WE.marker([30.058056, 31.228889]).addTo(earth);
-        marker2.bindPopup("<b>Cairo</b><br>Yay, you found me!", {maxWidth: 120, closeButton: false});
-		
-		var markerArray = [];
-       
-
-        earth.setView([51.505, 0], 2);
-        
-        //call load&reformat JSON function and give add_to earth_function a callback
-        load_and_reformat_sheet_json(add_to_earth);
-        
-      }
-      
-      function add_to_earth(output_json) 
+function initialize() 
       {
-      console.log(output_json);
-      
+        	var earth = new WE.map('earth_div');
+        	WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(earth);
+    		var markerArray = [];
+       		earth.setView([51.505, 0], 2);
+        
+        	//call load&reformat JSON function and give add_to earth_function a callback
+        	load_and_reformat_sheet_json(add_to_earth);
+        	//wait for callback  by load_and_reformat_sheet_json function
+        	function add_to_earth(output_json) //get JSON from google sheet via load_and_reformat_sheet_json
+      			{
+            		$.each(output_json,function(i,column_data)// loop through each array
+		 				{
+		 				var lng = column_data.locationx;
+						var lat = column_data.locationy;
+						var name = column_data.name;
+						var time = column_data.time;
+						var thisMarker = WE.marker([lat,lng]).addTo(earth);
+						thisMarker.bindPopup('<h3>' + name + '</h3>' + time, {maxWidth: 120, closeButton: true});
+						markerArray.push(thisMarker);
+
+		 				});
+      			}	
+        
       }
+      
+      
+      
+      
       
       
       
