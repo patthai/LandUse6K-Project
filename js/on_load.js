0ec76f41bddaf0e097fc6f85ab35049b805ba70c
pat <pp.psuwit@gmail.com>
2017-01-06 19:45:20 -0700

    Drop down is working
    Drop down is working

---
+++ js/on_load.js
@@ -0,0 +1,14 @@
+function on_load()
+{
+    load_all_project();
+
+
+
+}
+
+function load_ui()
+{
+    table();
+    timeline();
+    //earth_initialize();
+}
--- js/project_selection.js
+++ js/project_selection.js
@@ -3,37 +3,46 @@ function load_all_project() {
     var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/" + sheet + "/public/basic?alt=json";
     var project_lists="";
 
-    load_all_project.export_project_name = function export_project_name()
+
+    load_all_project.load_project = function load_project()
     {
+        sheet = 1;
         $('#project_choices').remove();
-        $('#list_of_projects').append(project_lists);
+        $('#dropdown_project').append("<select class=\"ui fluid search dropdown\"id =\"project_choices\"></select>");
         project_lists="";
 
+        load_all_project.loop_project = function loop_project() {
+            url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/" + sheet + "/public/basic?alt=json";
+            $.getJSON(url, function (data) // Get JSON from google sheet
+            {
+                project_lists = project_lists + "<option>" + data.feed.title.$t + "</option>";
+                sheet++;
+                load_all_project.loop_project();
+
+            })
+                .done(function() {
+                    //console.log('getJSON request succeeded!');
+                })
+                .fail(function(jqXHR, textStatus, errorThrown) {
+                    console.log(project_lists);
+                    $('#project_choices').append(project_lists);
+
+                })
+                .always(function() {
+                    //console.log('getJSON request ended!');
+                });
+
+
+
+
+        }
+        load_all_project.loop_project();
+
     }
 
-    load_all_project.loop_project = function loop_project()
-    {   url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/" + sheet + "/public/basic?alt=json";
-        $.getJSON(url, function(data) // Get JSON from google sheet
-        {//console.log(data.feed.title.$t);
-
-                if (data != null)
-                    {
-                        project_lists = project_lists + "<option id=\"project_choices\">" + data.feed.title.$t + "</option>";
-                        sheet++;
-                        load_all_project.loop_project();
-                        console.log(project_lists);
-                    }
-
-            load_all_project.export_project_name();
-            console.log("NULLLLLL");
-
-
-        });
-    }
-
-
-    load_all_project.loop_project();
+    load_all_project.load_project();
 
 
 
 }
+
